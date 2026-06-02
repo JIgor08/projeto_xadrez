c2a25387d8d5b9e93614bf0396da001b956d678b
igor <88968334+JIgor08@users.noreply.github.com>
2026-06-02 10:12:34 -0300

    second commit

--- board_layer/position.java
+++ board_layer/position.java
@@ -1,4 +1,12 @@
 package board_layer;
 public class position {
+    private int row;
+    private int column;
+
+    public void setValues(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
     
 }
